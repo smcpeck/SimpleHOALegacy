4b6b7067662c59c0b72352e73514b0fd93f5aede
Dan Murphy <dmurphy79@gmail.com>
2019-09-22 21:00:38 -0500

    Calendar styled somewhat

--- client/src/components/DashboardComponents/DBCalendar.jsx
+++ client/src/components/DashboardComponents/DBCalendar.jsx
@@ -10,36 +10,54 @@ class DBCalendar extends Component {
       events: []
     };
   }
-  componentDidMount(){
-    axios.get(`https://content.googleapis.com/calendar/v3/calendars/79ptp90b8asivdmhlp4ifl0fas@group.calendar.google.com/events?key=AIzaSyCc7TZ8lRqSWM6FA7xxCtlOV4gyGzhYASM`)
-    .then((res) => {
-      const events = res.data.items;
-      console.log(res.data.items[0].htmlLink);
-      this.setState({
-        events
+  componentDidMount() {
+    axios
+      .get(
+        `https://content.googleapis.com/calendar/v3/calendars/79ptp90b8asivdmhlp4ifl0fas@group.calendar.google.com/events?key=AIzaSyCc7TZ8lRqSWM6FA7xxCtlOV4gyGzhYASM`
+      )
+      .then(res => {
+        const events = res.data.items;
+        console.log(res.data.items[0].htmlLink);
+        this.setState({
+          events
+        });
       })
-    })
-    .catch((err) => console.error(err));
-
+      .catch(err => console.error(err));
   }
 
   render() {
     const { events } = this.state;
     return (
-      <div className="dashboard-card p-4">
+      <div className="dashboard-card p-4 mb-4">
         <h5>Upcoming Events</h5>
-        <Col>
-          {events.map(event => {
-            let htmlLink = event.h
-            return (
-            <Row>
-              <div><h6>{event.summary}</h6></div>
-              <div><h6> {event.start.dateTime.slice(5, 10)}</h6></div>
-              <Button outline color="info" onClick={() => window.location.href = event.htmlLink}>See more details!</Button> 
-            </Row>
-            )
-          })}
-        </Col>
+        {events.map(event => {
+          let htmlLink = event.h;
+          return (
+            <div className="mb-2">
+              <hr />
+              <Row>
+                <Col>
+                  <div>
+                    <h6>{event.summary}</h6>
+                  </div>
+                  <div>
+                    <h6> {event.start.dateTime.slice(5, 10)}</h6>
+                  </div>
+                </Col>
+                <Col>
+                  <Button
+                    color="primary"
+                    size="sm"
+                    className="float-right"
+                    onClick={() => (window.location.href = event.htmlLink)}
+                  >
+                    See more details!
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          );
+        })}
       </div>
     );
   }
