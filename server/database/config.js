bef6064b26b5317e5fdc7909166a3fea02245745
Shaun McPeck <smcpeck@gmail.com>
2021-01-20 08:27:46 -0600

    Closing string constant.

--- server/database/config.js
+++ server/database/config.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "hoa_database";
 const DB_USER = process.env.DB_USER || "hoa_user";
-const DB_PASSWORD = process.env.DB_PASSWORD || "hoa_password;
+const DB_PASSWORD = process.env.DB_PASSWORD || "hoa_password";
 
 const dialectOptions = {
     host: DB_HOST,
