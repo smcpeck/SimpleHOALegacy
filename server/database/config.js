5ebccddef338df051bf159d0d3d6fcc0ec3a0d5f
Shaun McPeck <smcpeck@gmail.com>
2021-01-20 08:18:27 -0600

    Tweak DB config to properly consume ENV vars

--- server/database/config.js
+++ server/database/config.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
-
-const { DATABASE_URL } = process.env;
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_NAME = process.env.DB_NAME || "hoa_database";
+const DB_USER = process.env.DB_USER || "hoa_user";
+const DB_PASSWORD = process.env.DB_PASSWORD || "hoa_password;
 
 const dialectOptions = {
-    ssl: {
-        require: true,
-        rejectUnauthorized: false,
-    },
+    host: DB_HOST,
+    dialect: process.env.DB_DIALECT || "mariadb"
 };
 
-module.exports.dbConnection = new Sequelize(DATABASE_URL, { dialectOptions });
+module.exports.dbConnection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, dialectOptions);
